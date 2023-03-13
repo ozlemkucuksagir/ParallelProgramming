48e8d662829f8a11606839acd965f40ac1e02553
Özlem Küçüksağır <ozlemkucuksagir2@gmail.com>
2023-03-13 22:59:08 +0300

    assignment 1

---
+++ 19050111021.c
@@ -0,0 +1,8 @@
+#include <19050111021.h>
+
+int main() {
+  // call a function in another file
+  myPrintHelloMake();
+
+  return(0);
+}
---
+++ hellofunc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <19050111021.h>
+
+void myPrintHelloMake(void) {
+
+  printf("Hello makefiles!\n19050111021\nOzlem KUCUKSAGIR\n");
+
+  return;
+}
